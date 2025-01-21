01d45e805607e155ca753f03165ad51827f9eab8
DimitarSr <dimitarsr2003@abv.bg>
2025-01-21 17:28:46 +0200

    add trending page rendering with API integration

---
+++ src/events/navigation-events.js
@@ -0,0 +1,44 @@
+import { q, setActiveNav } from './helpers.js';
+import {
+  TRENDING,
+  ABOUT,
+  FAVORITES,
+  MY_UPLOADS,
+  UPLOAD_GIF,
+  CONTAINER_SELECTOR,
+} from '../common/constants.js';
+import { toTrendingView } from '../views/trending-view.js';
+import { fetchTrending } from '../requests/request-service.js';
+
+export const loadPage = (page = '') => {
+  switch (page) {
+  case TRENDING:
+    setActiveNav(TRENDING);
+    return renderTrending();
+
+  case ABOUT:
+    setActiveNav(ABOUT);
+
+  case MY_UPLOADS:
+    setActiveNav(MY_UPLOADS);
+
+  case FAVORITES:
+    setActiveNav(FAVORITES);
+
+  case UPLOAD_GIF:
+    setActiveNav(UPLOAD_GIF);
+
+  default:
+    return null;
+  }
+};
+
+const renderTrending = async () => {
+  try {
+    const data = await fetchTrending();
+    q(CONTAINER_SELECTOR).innerHTML = toTrendingView(data);
+  } catch (error) {
+    console.error('Failed to load trending gifs:', error);
+
+  }
+};
